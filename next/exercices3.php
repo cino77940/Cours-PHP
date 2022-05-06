f2fa5b67ac1fb0006218e7e9dc5a5d1129c2927f
cino <cuneyt.tai@gmail.com>
2022-05-06 16:58:16 +0200

    Mise à jour du cours PHP

--- next/exercices3.php
+++ next/exercices3.php
@@ -2,20 +2,21 @@
     /*
         Créez une fonction qui permet d'afficher simplement le contenu d'une variable transmise
     */
-    
-    function afficheContenu($mavariable){
-        echo $mavariable;
+    function afficheContenu($maVariable){
+        echo $maVariable;
     }
-    afficheContenu ("Bonjour à tous !");
+
+    afficheContenu("Bonjour à tous !");
+    
+    
     /*
         Créez une fonction qui permet de multiplier deux nombres et RETOURNER (return) son résultat
     */
+    function multiplier($nb1 = 0, $nb2 = 0){
+        return $nb1 * $nb2;
+    }
 
-function multiplier($nb1 = 0, $nb2=0){
-    
-    return $nb1 * $nb2;
-}
-echo "<br/>" .multiplier (6, 2);
+    echo "<br/>".multiplier(6, 2);
 
     /*
         Créez une fonction qui permet de prendre 3 paramètres :
@@ -30,11 +31,11 @@ echo "<br/>" .multiplier (6, 2);
         2e info = 2
         3e info = 2
     */
-
-    function renvoiResultat ($nb1 = 0, $nb2= 0, $nb3 =0){
-        return($nb1 + $nb2) * $nb3;
+    function renvoiResultat($nb1 = 0, $nb2 = 0, $nb3 = 0){
+        return ($nb1 + $nb2) * $nb3;
     }
-    echo "<br/> Résultat du calcul :" .renvoiResultat (10, 20, 2);
+
+    echo "<br/>Résultat du calcul : ".renvoiResultat(10, 20, 2);
 
 
 ?>
---
+++ next/grosexercice.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+     Nous avons vu :
+            - Les variables
+            - Les tableaux
+            - Les conditions
+            - Les boucles
+            - Les inclusions de fichier
+            - Les fonctions
+            - La manipulation de formulaires
+            - L'upload de fichier
+
+
+    Écrire une boucle qui permet d’afficher le tableau de multiplication jusqu’à 10 d’un entier positif
+    choisi par l’utilisateur
+
+    Exemple : l'utilisateur choisir le nombre 5
+
+    Le programme doit afficher (grâce à une boucle) la table de multiplication :
+    1 x 5 = 5
+    2 x 5 = 10
+    3 x 5 = 15
+    ...
+    10 x 5 = 50
+
+    Si l'utilisateur entre 16
+    1 x 16 = 16
+    2 x 16 = 32
+    ..
+    10 x 16 = 160
+
+
+*/
+
+
+?>
